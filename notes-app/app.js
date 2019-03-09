f2b8f11075adf09980028ae9c88283d3ed835e1b
Rob Campbell <rdeancampbell@gmail.com>
2019-03-09 08:45:58 -0500

    Worked with yargs commands to parse command line arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,39 @@
-const getNotes = require('./notes.js');
-const yargs = require('yargs');
 const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
 // Customize yargs version
-yargs.version('1.1.0');
+yargs.version('1.1.0')
 
-console.log(yargs.argv);
+
+// add, remove, read, list 
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Adding a new note',
+  handler: ()=>console.log('Adding a new note')
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Removing a note',
+  handler: ()=>console.log('Removing note...')
+})
+
+// List command
+yargs.command({
+  command: 'list',
+  describe: 'Creates a list',
+  handler: ()=>console.log('Making a list')
+})
+
+// Read command
+yargs.command({
+  command: 'read',
+  describe: 'Read notes',
+  handler: ()=>console.log('Reading some notes')
+})
+
+console.log(yargs.argv)
