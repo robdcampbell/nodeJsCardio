10fd13843da30419feee66a154bb59781f453b6a
Rob Campbell <rdeancampbell@gmail.com>
2019-03-16 01:22:17 -0400

    Weather app fixed, added error handling for the API requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,19 +3,34 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/79494146fff4f77959b3266cf506a1ca/37.8267,-122.4233';
 
 
+// // Weather request from Darksky
 // request({ url: url,json:true }, (error,response)=>{
-//     // console.log(response.body.currently)
-//     const current = response.body.currently;
-//     console.log(`${response.body.daily.data[0].summary} It is currently ${current.temperature} degrees out. There is a ${current.precipProbability}% chance of rain.`)
+//     if(error){
+//         console.log('Unable to connect to weather service')
+//     } else if(response.body.error){
+//         console.log('Unable to find location')
+//     } else {
+//         const current = response.body.currently;
+//         console.log(`${response.body.daily.data[0].summary} It is currently ${current.temperature} degrees out. There is a ${current.precipProbability}% chance of rain.`)
+//     }
 // })
 
-// GEOcode using mapbox:
+// Geocoding:
+// Address -> Lat/Long -> Weather
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmRlYW5jYW1wYmVsbCIsImEiOiJjanQ2NnVtc3kwYnRqNDl0bWZ3dm14djZnIn0.2uwRjrVyvd0JdIqh8Zjirg&limit=1';
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/detroit.json?access_token=pk.eyJ1IjoicmRlYW5jYW1wYmVsbCIsImEiOiJjanQ2NnVtc3kwYnRqNDl0bWZ3dm14djZnIn0.2uwRjrVyvd0JdIqh8Zjirg&limit=1';
 
+
+// Geo request from mapbox
 request({url: geocodeURL, json:true}, (error,response)=>{
-    const latitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log(`${latitude} , ${longitude}`);
+    if(error){
+        console.log('Unable to connect to weather services');
+    } else if(response.body.features.length === 0){
+        console.log('Unable to find location, try again with different search term');
+    } else {
+        const latitude = response.body.features[0].center[1];
+        const longitude = response.body.features[0].center[0];
+        console.log(`${latitude} , ${longitude}`);
+    }
 })
 
