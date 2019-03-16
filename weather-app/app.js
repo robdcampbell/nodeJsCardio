0d629c2c56781e29de8d7afca656334adaa7069f
Rob Campbell <rdeancampbell@gmail.com>
2019-03-15 22:47:37 -0400

    fixed the default Mapbox access Token

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,29 +3,19 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/79494146fff4f77959b3266cf506a1ca/37.8267,-122.4233';
 
 
-request({ url: url,json:true }, (error,response)=>{
-    // console.log(response.body.currently)
-    const current = response.body.currently;
-    console.log(`${response.body.daily.data[0].summary} It is currently ${current.temperature} degrees out. There is a ${current.precipProbability}% chance of rain.`)
+// request({ url: url,json:true }, (error,response)=>{
+//     // console.log(response.body.currently)
+//     const current = response.body.currently;
+//     console.log(`${response.body.daily.data[0].summary} It is currently ${current.temperature} degrees out. There is a ${current.precipProbability}% chance of rain.`)
+// })
+
+// GEOcode using mapbox:
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmRlYW5jYW1wYmVsbCIsImEiOiJjanQ2NnVtc3kwYnRqNDl0bWZ3dm14djZnIn0.2uwRjrVyvd0JdIqh8Zjirg&limit=1';
+
+request({url: geocodeURL, json:true}, (error,response)=>{
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(`${latitude} , ${longitude}`);
 })
-
-
-
-//  HERE IT IS DUMMY: 
-
-// https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmRlYW5jYW1wYmVsbCIsImEiOiJjanQ2NnVtc3kwYnRqNDl0bWZ3dm14djZnIn0.2uwRjrVyvd0JdIqh8Zjirg
-
-
-// BROKEN ::::: NEED TO FIX 
-
-// const geocodeURL = 'https://api.mabox.com/geocoding/v5/mapbox.places/Los%Angeles.json?access_token=pk.eyJ1IjoicmRlYW5jYW1wYmVsbCIsImEiOiJjanQ2NnVtc3kwYnRqNDl0bWZ3dm14djZnIn0.2uwRjrVyvd0JdIqh8Zjirg'
-
-// request({ url: geocodeURL,json:true }, (error,response)=>{
-//     const data =  response.body;
-//     const latitude = data.latitude;
-//     const longitude = data.longitude;
-//     console.log(`My latitude is ${latitude} and my longitude is ${longitude}`);
-// });
-
-
 
