7d3193123059d5216352a21db478b3b52141a5a6
Rob Campbell <rcampbell1@gal-057049.local>
2019-03-18 16:59:13 -0400

    Began integrating forecast and geocode functions

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,33 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast.js');
 
-geocode('New York', (error, data) => {
-    console.log('Error', error);
-    console.log('Data',data);
-})
+// Goal: Accept location via comman line argument 
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+// 1. Access the command line argument without yargs
+// 2. Use the string value as the input for geocode
+// 3. Only geocode if a location was provided
+// 4. Test your work with a couple of locations
+
+// geocode('New York', (error, data) => {
+//     if(error){
+//       return console.log(error);
+//     }
+
+//     forecast(data.latitude, data.longitude, (error, forecastData) => {
+//        if(error){
+//            return console.log(error);
+//        }
+       
+//        console.log(data.location);
+//        console.log(forecastData);
+//     })
+// })
+
+
+const command = process.argv[2];
+
+if(typeof command === 'string'){
+    console.log('true');
+} else {
+    console.log('try something else');
+};
