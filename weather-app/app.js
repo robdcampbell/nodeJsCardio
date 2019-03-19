7ae87f62ee3e14ce9e33b21aef2e2bf6ac6cc86c
Rob Campbell <rdeancampbell@gmail.com>
2019-03-18 23:25:20 -0400

    Completed destructuring excerise in 5-es6-objects.js, moving on to destructuring in the weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,26 +8,27 @@ const forecast = require('./utils/forecast.js');
 // 3. Only geocode if a location was provided
 // 4. Test your work with a couple of locations
 
-// geocode('New York', (error, data) => {
-//     if(error){
-//       return console.log(error);
-//     }
+const location = process.argv[2];
 
-//     forecast(data.latitude, data.longitude, (error, forecastData) => {
-//        if(error){
-//            return console.log(error);
-//        }
-       
-//        console.log(data.location);
-//        console.log(forecastData);
-//     })
-// })
-
-
-const command = process.argv[2];
-
-if(typeof command === 'string'){
-    console.log('true');
+if(!location){
+    console.log('Please provide an address')
 } else {
-    console.log('try something else');
-};
+    geocode(location, (error, data) => {
+        if(error){
+          return console.log(error);
+        }
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+           if(error){
+               return console.log(error);
+           }
+           
+           console.log(data.location);
+           console.log(forecastData);
+        })
+    })
+}
+
+
+
+
