90761cc11f9948cd464d09c29ea470bab9d51d36
Rob Campbell <rdeancampbell@gmail.com>
2019-03-19 21:33:18 -0400

    Finished low-level practice of https requests WITHOUT using the Request Module

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -1,12 +1,25 @@
 const https = require('https');
 const url = `https://api.darksky.net/forecast/79494146fff4f77959b3266cf506a1ca/40,-75`;
 
-https.request(url, (response) => {
+const request = https.request(url, (response) => {
+    let data = '';
 
     response.on('data', (chunk) => {
+        data = data + chunk.toString();
+        console.log(chunk);
+    })
 
+    response.on('end', () => {
+        const body = JSON.parse(data);
+        console.log(body);
     })
 
 })
+
+request.on('error',(error) => {
+    console.log('An error', error);
+})
+
+request.end()
 
 // BEGIN AT 6:48
