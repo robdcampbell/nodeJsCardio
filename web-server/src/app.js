76e1ef5c1c25c21cd2bbc9f4b25f1897fa6baf70
Rob Campbell <rdeancampbell@gmail.com>
2019-03-21 00:09:25 -0400

    Begin Templating using handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,39 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDirectoryPath))
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs');
+app.set('views',viewsPath)
+
+// Setup Static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('',(req,res) => {
+  res.render('index',{
+    title: 'Weather',
+    name: 'Rob Campbell'
+  })
+})
+
+app.get('/about',(req,res) => {
+  res.render('about',{
+    title: 'About Me',
+    name: 'Rob Campbell'
+  })
+})
+
+app.get('/help',(req,res) => {
+  res.render('Help',{
+    title: 'Help',
+    name: 'Rob Campbell',
+    message: 'Help is on the way!'
+  })
+})
 
 app.get('/weather', (req,res) => {
   res.send({
