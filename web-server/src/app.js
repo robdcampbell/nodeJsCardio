c9533d7d048b56be2ec66dabcaa2c0696dc3c0b3
Rob Campbell <rcampbell1@gal-057049.local>
2019-03-21 16:11:42 -0400

    Moved utils containing geocode and forecast modules into web-server project files, began building the JSON HTTP endpoint with the requested location

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs')
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine','hbs');
 app.set('views',viewsPath)
+hbs.registerPartials(partialsPath);
 
 // Setup Static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -29,18 +32,61 @@ app.get('/about',(req,res) => {
 })
 
 app.get('/help',(req,res) => {
-  res.render('Help',{
+  res.render('help',{
     title: 'Help',
     name: 'Rob Campbell',
-    message: 'Help is on the way!'
+    helpText: 'Help is on the way!'
   })
 })
 
 app.get('/weather', (req,res) => {
+  
+  if(!req.query.address){
+    return res.send({
+      error: 'You must provide an address.'
+    })
+  }
+
   res.send({
     location: 'Detroit',
-    forecast: 'Clear Skies and Sunny'
+    forecast: 'Clear Skies and Sunny',
+    addres: req.query.address
   });
+})
+
+
+
+// PRODUCTS EXAMPLE
+app.get('/products', (req,res) => {
+
+    if(!req.query.search){
+      return res.send({
+        error: 'You must provide a search term'
+      })
+    }
+
+   console.log(req.query);
+   res.send({
+     products: []
+   })
+})
+
+  // Will match anything following '/help' (*wildcard within a particular pattern) 
+app.get('/help/*',(req,res) => {
+  res.render('404',{
+    title: '404: Page not found',
+    errorMessage: 'Help article not found!',
+    name: 'Rob Campbell'
+  })
+})
+
+// Will match anything (helpful for creating a 404 page, which is shown here)
+app.get('*', (req,res)=> {
+  res.render('404',{
+    title: '404: Page not found',
+    errorMessage: 'Whoops! 404: Page not found',
+    name: 'Rob Campbell'
+  })
 })
 
 app.listen(3000, () =>{
