72421e626df8c813bc6bebf3471fb7f873255280
Rob Campbell <rdeancampbell@gmail.com>
2019-03-23 10:00:04 -0400

    Wire up the client-side JS/form on the weatherApp index page to search for a specific location

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,15 @@
 console.log('Client side javascript is loaded');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
 
-fetch('http://localhost:3000/weather?address=detroit')
+
+// Event Listeners
+weatherForm.addEventListener('submit',(e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
 .then(((response)=>{
   response.json().then((data) => {
     if(data.error){
@@ -11,3 +20,9 @@ fetch('http://localhost:3000/weather?address=detroit')
     }
   })
 }))
+
+  
+  console.log(location);
+
+  search.value = '';
+})
