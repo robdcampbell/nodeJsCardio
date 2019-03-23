c3cccf61a6ba5e1974a346dd9f31a8b635dfe311
Rob Campbell <rdeancampbell@gmail.com>
2019-03-22 23:52:28 -0400

    Add fetch API to access forecast information on the client side

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,21 @@
 console.log('Client side javascript is loaded');
+
+fetch('http://puzzle.mead.io/puzzle')
+.then((response) => {
+  response.json().then((data)=>{
+    console.log(data);
+  })
+})
+
+
+fetch('http://localhost:3000/weather?address=detroit')
+.then(((response)=>{
+  response.json().then((data) => {
+    if(data.error){
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  })
+}))
