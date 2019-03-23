571ea2385b0aa6f0aeb0b730a2fcb0df44fdd966
Rob Campbell <rdeancampbell@gmail.com>
2019-03-23 15:25:41 -0400

    Completed front-end of WeatherApp. App is completely wired up and am now moving on to deployment

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,9 @@
 console.log('Client side javascript is loaded');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
 
 
 // Event Listeners
@@ -9,12 +12,18 @@ weatherForm.addEventListener('submit',(e) => {
 
   const location = search.value;
 
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = ''; 
+
   fetch(`http://localhost:3000/weather?address=${location}`)
 .then(((response)=>{
   response.json().then((data) => {
     if(data.error){
+      messageOne.textContent = data.error;
       console.log(data.error);
     } else {
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast; 
       console.log(data.location);
       console.log(data.forecast);
     }
