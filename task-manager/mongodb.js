2d9e4b133f6e355d553556a8026cfba7f0acdef5
Rob Campbell <rdeancampbell@gmail.com>
2019-03-29 07:20:18 -0400

    Setup mongodb.js to begin the CRUD app. Insert first document into task-manager

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -4,5 +4,17 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
-const database = 'task-manager';
+const databaseName = 'task-manager';
 
+MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
+  if(error){
+    return console.log('Unable to connect to database')
+  } 
+
+  const db = client.db(databaseName);
+
+  db.collection('users').insertOne({
+    name: "Rob",
+    age: 30
+  })
+})
