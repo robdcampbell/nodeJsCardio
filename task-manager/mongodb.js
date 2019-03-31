2fdf9263abac55b58fb26e400c93e2b44484a693
Rob Campbell <rdeancampbell@gmail.com>
2019-03-30 21:13:45 -0400

    Continue mongodb document inserting

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,20 +1,47 @@
-// CRUD create read update delete
+// // CRUD create read update delete
+
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+
+// const connectionURL = 'mongodb://127.0.0.1:27017';
+// const databaseName = 'task-manager';
+
+// MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
+//   if(error){
+//     return console.log('Unable to connect to database')
+//   } 
+
+//   const db = client.db(databaseName);
+
+//   db.collection('users').insertOne({
+//     name: "Rob",
+//     age: 30
+//   })
+// })
+
 
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const connectionURL = 'mongodb://127.0.0.1:27017';
-const databaseName = 'task-manager';
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
+const databaseName =  'task-manager';
+const connectionURL= 'mongodb://127.0.0.1:27017';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error,client)=>{
   if(error){
-    return console.log('Unable to connect to database')
-  } 
+    return console.log('Unable to connect to database!')
+  }
 
   const db = client.db(databaseName);
 
   db.collection('users').insertOne({
-    name: "Rob",
-    age: 30
+    name:'Andrew',
+    age: '27'
+  }, (error, result)=>{
+      if(error){
+        return console.log('Unable to insert user')
+      }
+      
+      console.log(result.ops);
   })
 })
