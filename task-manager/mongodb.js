d6829a5576bf6f5478e1915356bc17f38395fbc1
Rob Campbell <rdeancampbell@gmail.com>
2019-04-01 22:31:04 -0400

    Begin reading files using .findOne() and findMany()

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -7,10 +7,6 @@
 // Same using destructoring 
 const { MongoClient, ObjectID } = require('mongodb');
 
-const id = new ObjectID();
-console.log(id);
-console.log(id.getTimestamp());
-
 const databaseName =  'task-manager';
 const connectionURL= 'mongodb://127.0.0.1:27017';
 
@@ -21,56 +17,12 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error,client)=>{
 
   const db = client.db(databaseName);
 
-      db.collection('users').insertOne({
-        _id: id,
-        name:'Vikram',
-        age: '26'
-      }, (error, result) => {
-          if(error){
-            return console.log('Unable to insert user');
-          }
+  db.collection('users').findOne({name: 'Jen'}, (error, user)=> {
+    if(error){
+      return console.log('Unable to fetch');
+    }
 
-          console.log(result.ops)
-      })
-    
-      // db.collection('users').insertMany([
-      //   {
-      //     name: 'Jen',
-      //     age: 28
-      //   },
-      //   {
-      //     name: 'Gunther',
-      //     age: 26
-      //   }
-      // ], (error, result)=>{
-      //   if(error){
-      //     return console.log('Unable to insert documents!')
-      //   }
-
-      //   console.log(result.ops);
-      // })
-
-    // db.collection('tasks').insertMany([
-    //   {
-    //     description: 'Buy groceries',
-    //     completed: true
-    //   },
-    //   {
-    //     description:  'Make marathon playlist',
-    //     completed: false
-    //   },
-    //   {
-    //     description: 'Finish portfolio site',
-    //     completed: false
-    //   }
-    // ], (error,result)=>{
-    //   if(error){
-    //     return console.log('Unable to add tasks!')
-    //   }
-
-    //   console.log(result.ops)
-    //  })
-
-
-
+    console.log(user);
+  })
+      
 })
--- task-manager/practice.js
+++ task-manager/practice.js
@@ -11,4 +11,56 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error,client)=>{
     age: 30
   })
 
+  // db.collection('users').insertOne({
+      //   name:'Vikram',
+      //   age: '26'
+      // }, (error, result) => {
+      //     if(error){
+      //       return console.log('Unable to insert user');
+      //     }
+
+      //     console.log(result.ops)
+      // })
+    
+      // db.collection('users').insertMany([
+      //   {
+      //     name: 'Jen',
+      //     age: 28
+      //   },
+      //   {
+      //     name: 'Gunther',
+      //     age: 26
+      //   }
+      // ], (error, result)=>{
+      //   if(error){
+      //     return console.log('Unable to insert documents!')
+      //   }
+
+      //   console.log(result.ops);
+      // })
+
+    // db.collection('tasks').insertMany([
+    //   {
+    //     description: 'Buy groceries',
+    //     completed: true
+    //   },
+    //   {
+    //     description:  'Make marathon playlist',
+    //     completed: false
+    //   },
+    //   {
+    //     description: 'Finish portfolio site',
+    //     completed: false
+    //   }
+    // ], (error,result)=>{
+    //   if(error){
+    //     return console.log('Unable to add tasks!')
+    //   }
+
+    //   console.log(result.ops)
+    //  })
+
+
+
+
 })
