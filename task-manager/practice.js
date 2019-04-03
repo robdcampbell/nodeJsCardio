afe493bc407b61de170afaa0ff71c619d830c0e7
Rob Campbell <rdeancampbell@gmail.com>
2019-04-03 00:05:12 -0400

    Update practice.js with Create/Read methods in mongodb

--- task-manager/practice.js
+++ task-manager/practice.js
@@ -61,4 +61,30 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error,client)=>{
 
 
 
+    //   READ
+     // db.collection('users').findOne({_id: new ObjectID("5ca109a62810793e77941fe5")}, (error, user)=> {
+  //   if(error){
+  //     return console.log('Unable to fetch');
+  //   }
+
+  //   console.log(user);
+  // })
+
+  // db.collection('users').find({ age: 26 }).toArray((error,users) => {
+  //   console.log(users);
+  // })
+  // db.collection('users').find({ age: 26 }).toArray((error,count) => {
+  //   console.log(count);
+  // })
+
+  // db.collection('tasks').findOne({ _id: new ObjectID("5ca0fab0de7ddd3d78e6bef1") }, (error,task) =>{
+  //   console.log(task);
+  // })
+
+  // db.collection('tasks').find({completed: false}).toArray((error,tasks)=>{
+  //   console.log(tasks);
+  // })
+
+
+
 })
