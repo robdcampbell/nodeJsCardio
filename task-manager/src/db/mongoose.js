22acb13a1c6b3bb9d6447e4427a5fd1e61f3e5a2
Rob Campbell <rcampbell1@gal-057049.local>
2019-04-08 16:40:47 -0400

    Work with 'validator' and add validation/sanitation to mongoose.js

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,26 +1,82 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
-    useNewUrlParser: true,
-    useCreateIndex: true
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+  useNewUrlParser: true,
+  useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-  name: {
-    type: String
+const User = mongoose.model('User',{
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if(!validator.isEmail(value)){
+          throw new Error('Email is invalid')
+        }
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      trim:true,
+      minLength: 7,
+      validate(value){
+        if(value.includes('password')){
+          throw new Error('Cannot contain the word "Password"!')
+        }
+      }
+    },  
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if(value < 0){
+          throw new Error('Age must be a positive number')
+        }
+      }
+    }
+})
+
+// const me = new User({
+//   name: "   Rob    ",
+//   email: "MYEMAIL@MEAD.IO",
+//   password: "abc"
+// });
+
+// me.save().then((me)=>{
+//   console.log(me);
+// }).catch((error)=>{
+// console.log('Error: ', error)
+// });
+
+const Task = mongoose.model('Task', { 
+  description: {
+    type: String,
+    required: true,
+    trim: true
   },
-  age: {
-    type: Number
+  completed: {
+    type: Boolean,
+    default: false
   }
 })
 
-const me = new User({
-  name: 'Rob',
-  age: 30
+const task1 = new Task({
+  description: "        Take out the trash       "
 })
 
-me.save().then((me)=>{
-    console.log(me);
+task1.save().then((task1)=>{
+  console.log(task1)
 }).catch((error)=>{
-  console.log(`Error: ${error}`);
-})
+  console.log(error)
+});
+
+ 
