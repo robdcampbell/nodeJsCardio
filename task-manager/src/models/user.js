c61c96948e097f55c4304879d5a8d1696485c9f2
Rob Campbell <rdeancampbell@gmail.com>
2019-04-08 22:38:52 -0400

    refactor task-manager: mongoose.js, index.js, models/user.js

---
+++ task-manager/src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
+const User = mongoose.model('User',{
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if(!validator.isEmail(value)){
+        throw new Error('Email is invalid')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim:true,
+    minlength: 7,
+    validate(value){
+      if(value.includes('password')){
+        throw new Error('Cannot contain the word "Password"!')
+      }
+    }
+  },  
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if(value < 0){
+        throw new Error('Age must be a positive number')
+      }
+    }
+  }
+})
+
+module.exports = User
