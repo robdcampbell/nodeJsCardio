8757f32244c1c9eac58a5111729a2765f2f42b59
Rob Campbell <rdeancampbell@gmail.com>
2019-04-13 14:57:43 -0400

    explore promise chaining

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,20 +1,57 @@
-const doWorkPromise = new Promise((resolve,reject)=>{
-  setTimeout(()=>{
 
-    //resolve("It's Working!")
+const add = (a,b)=> {
+  return new Promise((resolve,reject) => {
+      setTimeout(()=>{
+         resolve(a+b);
+      },2000)
+  })
+}
 
-    reject('Nope nope nope!')
-  
-  }, 2000)
+add(1,2).then((sum)=>{
+  console.log(sum);
+
+  add(sum, 5).then((sum2)=>{
+    console.log(sum2);
+  }).catch((error)=>{
+    console.log(error);
+  })
+}).catch((error)=>{
+  console.log(error);
 })
 
-doWorkPromise
-  .then((result)=>{
-    console.log(`Hooray! ${result}`)
-  })
-  .catch((error)=>{
-    console.log(`Not today pal! ${error}`)
-  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// PROMISE EXAMPLE (basic)
+
+// const doWorkPromise = new Promise((resolve,reject)=>{
+//   setTimeout(()=>{
+
+//     //resolve("It's Working!")
+
+//     reject('Nope nope nope!')
+  
+//   }, 2000)
+// })
+
+// doWorkPromise
+//   .then((result)=>{
+//     console.log(`Hooray! ${result}`)
+//   })
+//   .catch((error)=>{
+//     console.log(`Not today pal! ${error}`)
+//   })
 
 
 
