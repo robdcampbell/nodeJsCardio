064c401e24528ca37c2094f49bc6b4052aa363fa
Rob Campbell <rdeancampbell@gmail.com>
2019-04-19 23:25:46 -0400

    continued Async/Await practice

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -10,3 +10,15 @@ Task.findByIdAndDelete('5cad6a30f827556977a28a7b').then((task)=>{
   console.log(e);
 })
 
+const deleteTaskAndCount = async (id) => {
+  const task = await Task.findOneAndDelete(id);
+  const count = await Task.countDocuments({id});
+  return count
+}
+
+
+deleteTaskAndCount('5cad6abddd1654698e12a799').then((count)=>{
+  console.log(count);
+}).catch((e)=>{
+  console.log(e);
+})
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -11,3 +11,16 @@ User.findByIdAndUpdate('5cac046ffe47986295b86e67', {age: 1}).then((user)=>{
 }).catch((e)=>{
   console.log(e);
 })
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age })
+  const count = await User.countDocuments( { age })
+  return count
+}
+
+updateAgeAndCount('5cac046ffe47986295b86e67', 2).then((count)=>{
+  console.log(count);
+}).catch((e)=>{
+  console.log(e);
+})
+
