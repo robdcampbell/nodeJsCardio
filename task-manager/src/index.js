b35672abf3f3b2ec6875a2ac06e9778a20b817b6
Rob Campbell <rdeancampbell@gmail.com>
2019-04-21 14:46:15 -0400

    Updating the 'PATCH' Request for Users in index.html of taskmanager. Updating the endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -52,6 +52,30 @@ app.get('/users', async (req,res)=>{
 
   })
 
+  app.patch('/users/:id', async (req,res)=>{
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name','email','password','age'];
+    const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
+
+    if(!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid Updates' })
+    }
+
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, 
+        { new: true, runValidators: true});
+
+        if(!user){
+          return res.status(404).send()
+        }
+
+        res.send(user);
+    } catch (e) {
+      res.status(400).send(e)
+    }
+  })
+
 // TASKS
 
 app.post('/tasks', async (req,res)=>{
