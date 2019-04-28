ff92f92d0801df9f0baa5590990244a30faf9907
Rob Campbell <rdeancampbell@gmail.com>
2019-04-27 21:35:57 -0400

    Continued implementing brcyptjs with Securely Storing passwords with Users

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-
-const User = mongoose.model('User',{
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true, 
     trim: true
   },
   email: {
@@ -40,5 +40,17 @@ const User = mongoose.model('User',{
     }
   }
 })
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
