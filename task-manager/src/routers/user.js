07ad7cb86889926f80f9cbf44934e6c7377a41f1
Rob Campbell <rdeancampbell@gmail.com>
2019-05-02 22:26:27 -0400

    continue practice with the Patch method

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -44,37 +44,37 @@ router.get('/users', async (req,res)=>{
 
   })
 
-  // router.patch('/users/:id', async (req,res)=>{
+  router.patch('/users/:id', async (req,res)=>{
 
-  //   const updates = Object.keys(req.body)
-  //   const allowedUpdates = ['name','email','password','age'];
-  //   const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name','email','password','age'];
+    const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
 
-  //   if(!isValidOperation) {
-  //     return res.status(400).send({ error: 'Invalid Updates' })
-  //   }
+    if(!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid Updates' })
+    }
 
-  //   try {
-  //     const user = await User.findById(req.params.id)
+    try {
+      const user = await User.findById(req.params.id)
 
-  //     updates.forEach((update)=> {
-  //       user[update] = req.body[update];
-  //     });
+      updates.forEach((update)=> {
+        user[update] = req.body[update];
+      });
 
-  //     await user.save();
+      await user.save();
       
-  //     // const user = await User.findByIdAndUpdate(req.params.id, req.body, 
-  //     //   { new: true, runValidators: true});
+      // const user = await User.findByIdAndUpdate(req.params.id, req.body, 
+      //   { new: true, runValidators: true});
 
-  //       if(!user){
-  //         return res.status(404).send()
-  //       }
+        if(!user){
+          return res.status(404).send()
+        }
 
-  //       res.send(user);
-  //   } catch (e) {
-  //     res.status(400).send(e)
-  //   }
-  // })
+        res.send(user);
+    } catch (e) {
+      res.status(400).send(e)
+    }
+  })
 
   router.delete('/users/:id', async(req,res)=> {
     try {
