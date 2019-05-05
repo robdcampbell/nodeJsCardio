622a08bde0c20acb09ebca88d7872d01a92f8c3e
Rob Campbell <rdeancampbell@gmail.com>
2019-05-04 23:21:55 -0400

    Add jsonWebToken as a dependency and begin integrating it

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,17 +18,10 @@ app.listen(port, ()=>{
   console.log(`Server is up on port ${port}`)
 })
 
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const myFunction  = async ()=> {
-  const password = 'red12345!'
-  const hashedPassword = await bcrypt.hash(password,8)
-
-  console.log(password);
-  console.log(hashedPassword);
-
-  const isMatch = await bcrypt.compare('red12345!', hashedPassword);
-  console.log(isMatch);
+    const token = jwt.sign({}, '')
 }
 
 myFunction();
