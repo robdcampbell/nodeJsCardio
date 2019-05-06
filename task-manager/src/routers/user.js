cb323c77dad591f6482581a032960fdcdabc7ebf
Rob Campbell <rdeancampbell@gmail.com>
2019-05-05 21:56:25 -0400

    Generate authenticication tokens in the create/login user methods

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -7,16 +7,21 @@ router.post('/users', async (req,res)=>{
 
   try {
     await user.save();
+    const token = await user.generateAuthToken()
+    res.send({ user, token });
     res.status(201).send(user);
   } catch (e) {
     res.status(400).send(e);
   }
 })
 
+
+
 router.post('/users/login', async (req,res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password);
-    res.send(user);
+    const token = await user.generateAuthToken()
+    res.send({ user, token });
   } catch (e){
     res.status(400).send();
   }
