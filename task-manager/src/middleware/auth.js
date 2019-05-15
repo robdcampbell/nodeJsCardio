7046af5ebff57ba567241bfcd74f34501c3023dd
Rob Campbell <rdeancampbell@gmail.com>
2019-05-14 23:45:17 -0400

    Fix jwt.verify passcode

--- task-manager/src/middleware/auth.js
+++ task-manager/src/middleware/auth.js
@@ -4,7 +4,10 @@ const User = require('../models/user')
 const auth = async (req,res,next) => {
   try {
     const token = req.header('Authorization').replace('Bearer', '');
-    const decoded = jwt.verify(token, 'thisismynewcourse');
+    
+    // FIX THIS ::
+
+    // const decoded = jwt.verify(token, 'thisismynewcourse');
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
     if (!user){
