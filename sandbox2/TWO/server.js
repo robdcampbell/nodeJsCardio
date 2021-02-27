d170cf52a068835d44c7b45cdea476d28417db59
robdcampbell <rdeancampbell@gmail.com>
2021-02-27 11:43:13 -0500

    add lodash and  express to project dependencies

--- sandbox2/TWO/server.js
+++ sandbox2/TWO/server.js
@@ -1,28 +1,48 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  // lodash
+  const num = _.random(0, 20);
+  console.log(num);
 
+  const greet = _.once(() => {
+    console.log("Hello");
+  });
+  greet();
+  greet();
+
+  // Set Header for the response
   res.setHeader("Content-Type", "text/html");
 
+  // create defaut path to access the html files, relative this server file
   let path = "./views/";
 
+  // Route Switch using NODE.JS (Before implementing Express.js)
   switch (req.url) {
-    case "./":
+    case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
-    case "./about":
+    case "/about":
       path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
 
   // send and html file
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(data);
     } else {
