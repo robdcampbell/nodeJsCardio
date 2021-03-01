b64c58e84012a5767390fe5131a15fdb4a9837ce
robdcampbell <rdeancampbell@gmail.com>
2021-03-01 13:01:17 -0500

    Saving progress before moving into using template engines

---
+++ sandbox2/TWO/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// listen for requests
+app.listen(PORT);
+
+//  Set endpoints (routes)
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile("./views/contact.html", { root: __dirname });
+});
+
+// Redirect - auto re-direct and sets the status code.
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 Page
+app.use((req, res) => {
+  res.status(404).sendFile("/views/404.html", { root: __dirname });
+});
