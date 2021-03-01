2c56ca6f48011dfa03fbe019bfba22262f495cbd
robdcampbell <rdeancampbell@gmail.com>
2021-03-01 15:34:53 -0500

    CSS and ejs refactor setup for overall blog

--- sandbox2/TWO/app.js
+++ sandbox2/TWO/app.js
@@ -11,18 +11,23 @@ app.listen(PORT);
 
 //  Set endpoints (routes)
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    { title: "Yoshi finds eggs", snippet: "Lorem ipsum dolor sit amet." },
+    { title: "Mario finds stars", snippet: "Lorem ipsum dolor sit amet." },
+    { title: "How to defeat Bowser", snippet: "Lorem ipsum dolor sit amet." },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create a new Blog" });
 });
 
 // 404 Page
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "Oops!" });
 });
