ab5cf17db93bf792a45f3cbc7cc1c42086204f36
robdcampbell <rdeancampbell@gmail.com>
2021-03-24 10:07:47 -0400

    Express Cardio in Sandbox3

--- sandbox3/2-express/app.js
+++ sandbox3/2-express/app.js
@@ -1,23 +1,19 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
+const PORT = 5000;
 
-const homePage = readFileSync("./navbar-app/index.html");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About page</h1>");
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>404, page not found</h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.send("Home Page: Hello World");
 });
 
-server.listen(5000, () => console.log("Ok Alright"));
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
