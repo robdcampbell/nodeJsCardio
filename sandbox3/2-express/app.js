215b2a72089805286adfab38284a4f9cbf3b4c9b
robdcampbell <rdeancampbell@gmail.com>
2021-03-25 07:43:53 -0400

    continue with middleware cardio

--- sandbox3/2-express/app.js
+++ sandbox3/2-express/app.js
@@ -2,18 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-
 // req => middleware => res
 
+// 6:21
 
-app.get('/', (req, res)=>{
-  res.send('2home');
-})
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
 
-app.get('/about', (req, res)=>{
-  res.send('about');
-})
+app.get("/", logger, (req, res) => {
+  res.send("Home");
+});
 
+app.get("/about", (req, res) => {
+  res.send("about");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listenin on port ${PORT}...`);
