0665dda399aa1711347c904a90e88e18e840db0f
robdcampbell <rdeancampbell@gmail.com>
2021-03-30 15:12:02 -0400

    Continued POST and PUT cardio with postman and express endpoints

--- sandbox3/2-express/app.js
+++ sandbox3/2-express/app.js
@@ -7,6 +7,8 @@ let { people } = require("./data");
 app.use(express.static("./methods-public"));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
+//parse JSON
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(`iTTS a MEEEE MARIOOOOO`);
@@ -14,10 +16,27 @@ app.get("/", (req, res) => {
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, people });
+  console.log("people requested....");
 });
 
 app.post("/api/people", (req, res) => {
-  res.status(200).json({ success: true, people });
+  const { name } = req.body;
+  if (!name) {
+    res
+      .status(400)
+      .json({ success: false, msg: "Please provide a name value" });
+  }
+  res.status(201).json({ success: true, person: name });
+});
+
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res
+      .status(400)
+      .json({ success: false, msg: "Please provide a name value" });
+  }
+  res.status(201).json({ success: true, data: [...people, name] });
 });
 
 app.post("/login", (req, res) => {
--- sandbox3/2-express/test.js
+++ sandbox3/2-express/test.js
@@ -24,4 +24,16 @@ function duplicateZero(arr) {
   //return arr.join("").replace(/0/g, "00").split("");
 }
 
-console.log(duplicateZero(arr));
+// console.log(duplicateZero(arr));
+
+var twoSum = function (nums, target) {
+  for (i = 0; i < nums.length; i++) {
+    for (j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([2, 7, 11, 15], 9));
