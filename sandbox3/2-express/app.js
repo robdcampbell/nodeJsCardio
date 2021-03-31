23d2e0333e26f3253fd6a3915b3808367e4fd800
robdcampbell <rdeancampbell@gmail.com>
2021-03-31 12:00:31 -0400

    Delete, put, and post Node.js Cardio

--- sandbox3/2-express/app.js
+++ sandbox3/2-express/app.js
@@ -48,6 +48,39 @@ app.post("/login", (req, res) => {
   }
 });
 
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id: ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  return res.status(201).json({ success: true, data: [...newPeople] });
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id: ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id)
+  );
+  return res.status(200).json({ success: true, data: newPeople });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listenin on port ${PORT}...`);
 });
