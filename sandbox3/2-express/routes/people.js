f812fa93d1f7ce73531b7b67d5c624eb839f6353
robdcampbell <rdeancampbell@gmail.com>
2021-03-31 23:23:15 -0400

    Express Router re-factor

--- sandbox3/2-express/routes/people.js
+++ sandbox3/2-express/routes/people.js
@@ -8,14 +8,18 @@ const {
   deletePerson,
 } = require("../controllers/people");
 
-router.get("/", getPeople);
+// OPTION 1
+/*
+  router.get("/", getPeople);
+  router.post("/", createPerson);
+  router.post("/postman", createPersonPostman);
+  router.put("/:id", updatePerson);
+  router.delete("/:id", deletePerson);
+*/
 
-router.post("/", createPerson);
-
-router.post("/postman", createPersonPostman);
-
-router.put("/:id", updatePerson);
-
-router.delete("/:id", deletePerson);
+// OPTION 2
+router.route("/").get(getPeople).post(createPerson);
+router.router("/postman").post(createPersonPostman);
+router.router("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
