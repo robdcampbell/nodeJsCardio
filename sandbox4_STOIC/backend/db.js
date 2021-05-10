ac7f298b9847ebe05dca90b96718c36ccad271c5
robdcampbell <rdeancampbell@gmail.com>
2021-05-10 17:46:46 -0400

    Connect Db and set up basic CRUD routes

---
+++ sandbox4_STOIC/backend/db.js
@@ -0,0 +1,13 @@
+// Database connect
+
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: "postgres",
+  password: "",
+  database: "todo_database",
+  host: "localhost",
+  port: 5432,
+});
+
+module.exports = pool;
--- sandbox4_STOIC/backend/server.js
+++ sandbox4_STOIC/backend/server.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const pool = require("./db");
 
 app.use(express.json());
+
+// ROUTES //
+
+// get all todos
+
+// get a todo
+
+// create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    //await req.body()
+    console.log(req.body);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// update a todo
+
+// delete a todo
 
 app.get("/", (req, res) => {
   res.json({ message: "Go Time!" });
