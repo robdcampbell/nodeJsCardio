ffb88b75cb35db3c9c5388e5d002f9a648243c9a
robdcampbell <rdeancampbell@gmail.com>
2021-05-11 10:30:17 -0400

    updat get and post routes in sandbox4

--- sandbox4_STOIC/backend/server.js
+++ sandbox4_STOIC/backend/server.js
@@ -8,14 +8,39 @@ app.use(express.json());
 // ROUTES //
 
 // get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
 
-// get a todo
+    res.status(200).json(allTodos.rows);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// get a specific todo
+app.get("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id=($1)", [
+      id,
+    ]);
+    res.status(200).json(todo.rows[0]);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // create a todo
 app.post("/todos", async (req, res) => {
   try {
     //await req.body()
-    console.log(req.body);
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+    res.status(200).json(newTodo.rows[0]);
   } catch (err) {
     console.log(err);
   }
